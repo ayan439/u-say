9626294c42ef5027ccb8a1625f1cec81f9ac0bf9
MOHD AYAN AKHTAR <ayanakhtar72@gmail.com>
2025-11-14 01:07:26 +0530

    Update agoraToken.js

--- src/agoraToken.js
+++ src/agoraToken.js
@@ -1,21 +1,21 @@
-// agoraToken.js
 import pkg from "agora-access-token";
 const { RtcTokenBuilder, RtcRole } = pkg;
 
 export function generateRtcToken(channelName, uid, ttlSeconds = 3600) {
   const appID = process.env.AGORA_APP_ID;
   const appCertificate = process.env.AGORA_APP_CERT;
-  const role = RtcRole.PUBLISHER; // user will publish/subscribe to audio
+
+  const role = RtcRole.PUBLISHER;
   const currentTimestamp = Math.floor(Date.now() / 1000);
   const privilegeExpireTs = currentTimestamp + ttlSeconds;
-  const numericUid = Number(uid);
-  const token = RtcTokenBuilder.buildTokenWithUid(
+
+  return RtcTokenBuilder.buildTokenWithUid(
     appID,
     appCertificate,
     channelName,
-    numericUid,
+    Number(uid),
     role,
     privilegeExpireTs
   );
-  return token;
 }
+
